56d2df26c48bd0461f6c01a6adc456b35183dee0
Patrick Catanzariti <pcatanzariti@gmail.com>
2015-10-12 10:15:58 +1100

    A working VR demo with a fancy lamp

---
+++ Assets/Lamp.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+public class Lamp : MonoBehaviour {
+	public Renderer skybox;
+	public Material sky1;
+	public Material sky2;
+	public Material sky3;
+	public Material sky4;
+	private int currentSky = 0;
+	
+	void Start () {
+	}
+	
+	public void rubLamp() {
+		GetComponent<ParticleSystem>().Emit(10); 
+	}
+	
+	public void stareAtLamp() {
+		switch (currentSky) {
+		case 0:
+			skybox.material = sky2;
+			currentSky++;
+			break;
+		case 1:
+			skybox.material = sky3;
+			currentSky++;
+			break;
+		case 2:
+			skybox.material = sky4;
+			currentSky++;
+			break;
+		case 3:
+			skybox.material = sky1;
+			currentSky = 0;
+			break;
+		}
+	}
+}
---
+++ README.md
@@ -0,0 +1,12 @@
+Google Cardboard Unity SDK Demo
+========
+
+This is a demo to show how the basics of to use the Google Cardboard Unity SDK to build VR apps!
+
+If this code helps you make something pretty cool, please do let me know! I'm on Twitter at @thatpatrickguy, or you can find me at http://www.patrickcatanzariti.com or http://www.patcat.com.
+
+Keen for more of this sort of thing? This demo is part of an article at http://www.sitepoint.com. I've got a range of different IoT/wearable/VR/AR demos up there!
+
+I've also got a site dedicated to developer resources for emerging tech like the IoT, wearables, VR, AR and artificial intelligence over at http://www.devdiner.com :)
+
+PatCat
